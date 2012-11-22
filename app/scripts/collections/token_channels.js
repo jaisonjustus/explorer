b41e06d22ab79778ea59f430f4700602077f1eab
Jon Maim <jonathan.maim@gmail.com>
2012-11-22 18:25:34 +0530

    View settings: default, all, trashed.

--- app/scripts/collections/token_channels.js
+++ app/scripts/collections/token_channels.js
@@ -1,14 +1,14 @@
-define(['jquery', 'underscore', 'backbone', 'channels'], function($, _, Backbone, Channels) {
+define(['jquery', 'underscore', 'backbone', 'channels', 'state'], function($, _, Backbone, Channels, state) {
   'use strict';
 
   return Channels.extend({
     url: function(){
       $.ajaxSetup({
         beforeSend: _.bind(function(xhr){
-          xhr.setRequestHeader('Authorization', this.token);
+          //xhr.setRequestHeader('Authorization', this.token);
         }, this)
       });
-      return this.baseApiUrl+'/channels';
+      return this.baseApiUrl+'/channels?state='+state.get('state')+'&auth='+this.token;
     }
   });
 
--- app/scripts/models/folder.js
+++ app/scripts/models/folder.js
@@ -11,6 +11,14 @@ define(['underscore', 'backbone'], function(_, Backbone) {
     , initialize: function(){
       var id = this.get('id');
     }
+    , url: function(){
+      $.ajaxSetup({
+        beforeSend: _.bind(function(xhr){
+          //xhr.setRequestHeader('Authorization',this.token);
+        }, this)
+      });
+      return this.collection.baseApiUrl+'/'+this.collection.channelId+'/folders/'+this.id+'?auth='+this.collection.token;
+    }
   });
 
 });
