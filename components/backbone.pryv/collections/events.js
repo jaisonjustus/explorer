732f5ff5ac88aff6ae3de2b1ffaa529ae2702414
Barbara Maim <maim.barbara@gmail.com>
2013-01-23 17:01:52 +0530

    v0.1.14

--- components/backbone.pryv/collections/events.js
+++ components/backbone.pryv/collections/events.js
@@ -7,6 +7,9 @@ define(['underscore', 'backbone', 'event', 'duration_event', 'state', 'pryv'], f
     , channelId: '-1'
     , token: null
     , baseApiUrl: null
+    , fromTime: null
+    , toTime: null
+    , modifiedSince: null
     /* Methods. */
     , model: function(attrs, options){
       if(attrs.duration === undefined ){
@@ -24,6 +27,11 @@ define(['underscore', 'backbone', 'event', 'duration_event', 'state', 'pryv'], f
       this.token = options.token;
       this.baseApiUrl = options.baseApiUrl;
       this.onlyFolders = options.onlyFolders || [];
+      this.fromTime = options.fromTime;
+      this.toTime = options.toTime;
+      if (options.modifiedSince){
+        this.modifiedSince = options.modifiedSince;
+      }
     } 
     , stopCurrentEvent: function(cb){
       var url = this.baseApiUrl+'/'+this.channelId+'/events/stop?auth='+encodeURIComponent(this.token);
@@ -38,6 +46,15 @@ define(['underscore', 'backbone', 'event', 'duration_event', 'state', 'pryv'], f
         for(var i = 0; i < this.onlyFolders.length; ++i){
           partialUrl += '&onlyFolders['+i+']='+this.onlyFolders[i];
         }
+      }
+      if(this.fromTime){
+        partialUrl += '&fromTime='+this.fromTime;
+      }
+      if(this.toTime){
+        partialUrl += '&toTime='+this.toTime;
+      }
+      if(this.modifiedSince){
+        partialUrl += '&modifiedSince='+this.modifiedSince;
       }
       return this.baseApiUrl+'/'+this.channelId+'/events?state='+state.get('state')+'&auth='+encodeURIComponent(this.token)+partialUrl;
     }
